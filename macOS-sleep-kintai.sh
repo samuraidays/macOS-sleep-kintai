bd264a41642f19199a8097b8bbd690752bd19c23
takashi hasegawa <pocky@C02SM65PGTFJ.local>
2020-10-19 00:35:54 +0900

    file uniq

--- macOS-sleep-kintai.sh
+++ macOS-sleep-kintai.sh
@@ -1,28 +1,47 @@
-#!/bin/bash
+#!/bin/bash -e
 # スリープログ(pmset -g log)のうち、ディスプレイのON/OFFを勤怠に使う
 # その日の最初の"Display is turned on"→勤務開始
 # その日の最後の"Display is turned off"→勤務終了
 
+IFS=$'\n'
 # 設定
 ## ファイルの最大行数
-max_row=200
+max_row=10
 ## ログファイル
 logfile="/tmp/monthly-kintai.log"
+dlog="/tmp/daily-kintai.log"
 
-yesterday=$(date -v -1d +"%Y-%m-%d")
 # 昨日のディスプレイのON/OFFログを取得する
-logs=`pmset -g log | grep "Display is turned" | grep $yesterday | cut -d ' ' -f -15`
-echo "$logs" > /tmp/daily-kintai.log
+#logs=`pmset -g log | grep "Display is turned" | grep $yesterday | cut -d ' ' -f -15`
+dates=`pmset -g log | grep "Display is turned" | cut -d ' ' -f -1 | uniq`
+
+sort $logfile > $logfile
+
+for date in $dates;
+do
+    logs=`pmset -g log | grep "Display is turned" | grep $date | cut -d ' ' -f -15`
+    start=`echo "$logs" | grep "Display is turned on" | head -n 1 | cut -d ' ' -f -2`
+    end=`echo "$logs" | grep "Display is turned off" | tail -n 1 | cut -d ' ' -f -2`
+
+    cat $logfile | grep $start > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo $start start >> $logfile
+    fi
+
+    cat $logfile | grep $end > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo $end end >> $logfile
+    fi
+done
 
 # ファイルの行数を取得
-file_row=`cat $logfile | wc -l | /usr/bin/awk '{print $1}'`
-
-if [ $file_row -ge $max_row ]; then
-    sed -i -e '1,2d' $logfile
-    echo "$logs" | grep "Display is turned on" | head -n 1 | cut -d ' ' -f -2 >> $logfile
-    echo "$logs" | grep "Display is turned off" | tail -n 1 | cut -d ' ' -f -2 >> $logfile
-else
-    echo "$logs" | grep "Display is turned on" | head -n 1 | cut -d ' ' -f -2 >> $logfile
-    echo "$logs" | grep "Display is turned off" | tail -n 1 | cut -d ' ' -f -2 >> $logfile
-fi
+#file_row=`cat $logfile | wc -l | /usr/bin/awk '{print $1}'`
+#if [ $file_row -ge $max_row ]; then
+#    row=`expr $file_row - $max_row`
+#    echo $row
+#    str=1,$row
+#    str2=d
+#    str=$str$str2
+#    sed -i -e $str $logfile
+#fi
 
